0f922b793bac3861b54d2f0f5bf81e9bde7a4277
Uzonyi Béla <uzonyib@gmail.com>
2014-12-17 21:37:01 +0100

    Dependency versions updated.

--- jreader-dao/src/test/java/jreader/dao/impl/AbstractDaoTest.java
+++ jreader-dao/src/test/java/jreader/dao/impl/AbstractDaoTest.java
@@ -6,20 +6,26 @@ import org.testng.annotations.BeforeMethod;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalMemcacheServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.util.Closeable;
 
 public abstract class AbstractDaoTest {
     
     private final LocalServiceTestHelper helper = new LocalServiceTestHelper(
-            new LocalDatastoreServiceTestConfig(),
+            new LocalDatastoreServiceTestConfig().setDefaultHighRepJobPolicyUnappliedJobPercentage(1.0f),
             new LocalMemcacheServiceTestConfig());
+    
+    private Closeable session;
     
     @BeforeMethod
     public void setUpHelper() {
         helper.setUp();
+        session = ObjectifyService.begin();
     }
 
     @AfterMethod
     public void tearDownHelper() {
+        session.close();
         helper.tearDown();
     }
 
