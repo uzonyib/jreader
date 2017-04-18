601245cec3ae593c3f6ac89c2ffe59ddb84d1ddf
uzonyib <uzonyib@gmail.com>
2017-04-18 20:51:12 +0200

    Java8 upgrade.

--- jreader-web/src/main/java/jreader/web/config/WebConfig.java
+++ jreader-web/src/main/java/jreader/web/config/WebConfig.java
@@ -10,8 +10,8 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 import jreader.domain.Role;
 import jreader.web.interceptor.AuthorizationInterceptor;
@@ -26,7 +26,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Bean
     public ViewResolver viewResolver() {
-        final UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
+        final InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setViewClass(JstlView.class);
         viewResolver.setPrefix("/WEB-INF/jsp/");
         viewResolver.setSuffix(".jsp");
