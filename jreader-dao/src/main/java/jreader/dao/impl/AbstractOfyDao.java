7ddaab7c59db7269467dc78e6d2fe6d29686529d
uzonyib <uzonyib@gmail.com>
2017-05-01 17:22:35 +0200

    Refactoring for Java8.

--- jreader-dao/src/main/java/jreader/dao/impl/AbstractOfyDao.java
+++ jreader-dao/src/main/java/jreader/dao/impl/AbstractOfyDao.java
@@ -1,36 +1,32 @@
 package jreader.dao.impl;
 
+import java.util.Arrays;
 import java.util.Collection;
-
-import jreader.dao.OfyDao;
-import jreader.domain.Archive;
-import jreader.domain.ArchivedPost;
-import jreader.domain.Feed;
-import jreader.domain.Post;
-import jreader.domain.FeedStat;
-import jreader.domain.Subscription;
-import jreader.domain.Group;
-import jreader.domain.User;
+import java.util.List;
 
 import com.googlecode.objectify.Key;
 import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.ObjectifyFactory;
 import com.googlecode.objectify.ObjectifyService;
-import com.googlecode.objectify.VoidWork;
-import com.googlecode.objectify.Work;
+
+import jreader.dao.OfyDao;
+import jreader.domain.Archive;
+import jreader.domain.ArchivedPost;
+import jreader.domain.Feed;
+import jreader.domain.FeedStat;
+import jreader.domain.Group;
+import jreader.domain.Post;
+import jreader.domain.Subscription;
+import jreader.domain.User;
 
 abstract class AbstractOfyDao<EntityType> implements OfyDao<EntityType> {
 
+    private static final List<Class<?>> DOMAIN_CLASSES = Arrays.asList(
+            User.class, Group.class, Subscription.class, Feed.class, Post.class, FeedStat.class, Archive.class, ArchivedPost.class);
+
     static {
         final ObjectifyFactory factory = factory();
-        factory.register(User.class);
-        factory.register(Group.class);
-        factory.register(Subscription.class);
-        factory.register(Feed.class);
-        factory.register(Post.class);
-        factory.register(FeedStat.class);
-        factory.register(Archive.class);
-        factory.register(ArchivedPost.class);
+        DOMAIN_CLASSES.forEach(clazz -> factory.register(clazz));
     }
 
     private static ObjectifyFactory factory() {
@@ -44,46 +40,28 @@ abstract class AbstractOfyDao<EntityType> implements OfyDao<EntityType> {
     @Override
     public EntityType save(final EntityType entity) {
         final Objectify ofy = getOfy();
-        return ofy.transact(new Work<EntityType>() {
-            @Override
-            public EntityType run() {
-                final Key<EntityType> key = ofy.save().entity(entity).now();
-                return ofy.load().key(key).now();
-            }
+        return ofy.transact(() -> {
+            final Key<EntityType> key = ofy.save().entity(entity).now();
+            return ofy.load().key(key).now();
         });
     }
 
     @Override
     public void saveAll(final Collection<EntityType> entities) {
         final Objectify ofy = getOfy();
-        ofy.transact(new VoidWork() {
-            @Override
-            public void vrun() {
-                ofy.save().entities(entities).now();
-            }
-        });
+        ofy.transact(() -> ofy.save().entities(entities).now());
     }
 
     @Override
     public void delete(final EntityType entity) {
         final Objectify ofy = getOfy();
-        ofy.transact(new VoidWork() {
-            @Override
-            public void vrun() {
-                ofy.delete().entity(entity).now();
-            }
-        });
+        ofy.transact(() -> ofy.delete().entity(entity).now());
     }
 
     @Override
     public void deleteAll(final Collection<EntityType> entities) {
         final Objectify ofy = getOfy();
-        ofy.transact(new VoidWork() {
-            @Override
-            public void vrun() {
-                ofy.delete().entities(entities).now();
-            }
-        });
+        ofy.transact(() -> ofy.delete().entities(entities).now());
     }
 
 }
